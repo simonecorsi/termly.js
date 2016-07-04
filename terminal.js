576ac9813c0f407f5de7d09bc34d8f60ee7a2c6f
Kirkhammetz <Neofrascati@hotmail.it>
2016-07-04 11:28:04 +0200

    v

--- terminal.js
+++ terminal.js
@@ -1,12 +1,12 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports={
   "name": "browser-terminal.js",
-  "version": "0.2.0",
+  "version": "0.2.1",
   "description": "Simple Browser Terminal in pure js for presentational purpose",
   "main": "terminal.js",
   "scripts": {
     "start": "npm run build",
-    "build": " browserify ./src/terminal.js -o ./terminal.js && uglifyjs terminal.js > terminal.min.js",
+    "build": "browserify ./src/terminal.js -o ./terminal.js && uglifyjs terminal.js > terminal.min.js",
     "watch": "watchify --verbose --debug ./src/terminal.js -o 'uglifyjs -cm > terminal.min.js'"
   },
   "keywords": [
